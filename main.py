ce210c68ea6bb7bead15e436263342e105266e9a
Ogolknev Nikita <ogolknev257@mail.ru>
2023-09-11 20:31:09 +0300

    Add data reader and drawer

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def data_reader(path: str):
+    data_obj = open(path)
+    data_str = data_obj.readline()
+    data_list = list()
+    cntr = 0
+    while data_str:
+        data_list.append(list())
+        data_list[cntr].append(list(float(x) for x in ''.join(data_str).split(' ')))
+        data_str = data_obj.readline()
+        data_list[cntr].append(list(float(y) for y in ''.join(data_str).split(' ')))
+        data_str = data_obj.readline()
+        data_str = data_obj.readline()
+        cntr += 1
+    data_obj.close()
+    return data_list
+
+def data_drawer(path: str, mode):
+    data = data_reader(path)
+    for i in range(len(data)):
+        if mode == 'a' or i + 1 in list(int(el) for el in mode.split(' ')):
+            plt.scatter(data[i][0], data[i][1])
+    plt.show()
+
+data_drawer('data.txt', input('Ведите количество или номер(a) графика: '))
